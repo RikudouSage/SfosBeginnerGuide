0c0d541ccf1756c398cdf87a9561bb31e071f3cd
Dominik Chrastecky <dominik@chrastecky.cz>
2025-12-30 12:19:46 +0100

    Add empty untranslated files

--- harbour-beginner-guide.pro
+++ harbour-beginner-guide.pro
@@ -44,7 +44,9 @@ CONFIG += sailfishapp_i18n
 # planning to localize your app, remember to comment out the
 # following TRANSLATIONS line. And also do not forget to
 # modify the localized app name in the the .desktop file.
-TRANSLATIONS += translations/harbour-beginner-guide-de.ts
+TRANSLATIONS += translations/harbour-beginner-guide-de.ts \
+                translations/harbour-beginner-guide-nb.ts \
+                translations/harbour-beginner-guide-cs.ts
 
 HEADERS += \
     src/appsettings.h \
---
+++ translations/harbour-beginner-guide-cs.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="8"/>
+        <source>My Cover</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LanguageLoader</name>
+    <message>
+        <location filename="../qml/pages/LanguageLoader.qml" line="10"/>
+        <source>Loading...</source>
+        <extracomment>Inside a loader</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ReaderPage</name>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="67"/>
+        <source>The requested app is not installed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="68"/>
+        <source>This type of link is not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="69"/>
+        <source>Could not open the link.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="70"/>
+        <location filename="../qml/pages/ReaderPage.qml" line="71"/>
+        <source>It seems you don&apos;t have %1 on your device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="70"/>
+        <source>Jolla Store</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="71"/>
+        <source>Storeman</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="87"/>
+        <source>Loading...</source>
+        <extracomment>Inside a loader</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="101"/>
+        <source>Settings</source>
+        <extracomment>Pull down menu item</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="108"/>
+        <source>Tutorial</source>
+        <extracomment>PUll down menu item</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="125"/>
+        <source>The requested page wasn&apos;t found, please try again later</source>
+        <extracomment>Error message</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReaderPage.qml" line="131"/>
+        <source>There was an error while loading the page, please try again later</source>
+        <extracomment>Error message</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="25"/>
+        <source>Settings</source>
+        <extracomment>Page title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="31"/>
+        <source>Automatic</source>
+        <extracomment>As in automatic language selection</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="37"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
