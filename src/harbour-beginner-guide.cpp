9f61cfbedc34c8b4aa146c03f79d2f793b7249e7
Dominik Chrastecky <dominik@chrastecky.cz>
2025-12-30 13:09:20 +0100

    Try manual lang install

--- src/harbour-beginner-guide.cpp
+++ src/harbour-beginner-guide.cpp
@@ -3,9 +3,12 @@
 #endif
 
 #include <QScopedPointer>
+#include <QCoreApplication>
 #include <QQuickView>
 #include <QQmlContext>
 #include <QGuiApplication>
+#include <QLocale>
+#include <QDebug>
 
 #include <sailfishapp.h>
 
@@ -13,11 +16,27 @@
 #include "intl.h"
 #include "linkhandler.h"
 
+// todo don't hardcode
+constexpr auto TRANSLATION_INSTALL_DIR = "/usr/share/harbour-beginner-guide/translations";
+
 int main(int argc, char *argv[])
 {
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
-    QScopedPointer<QQuickView> v(SailfishApp::createView());
 
+    QTranslator *defaultLang = new QTranslator(app.data());
+    if (!defaultLang->load("harbour-beginner-guide-en", TRANSLATION_INSTALL_DIR)) {
+        qWarning() << "Could not load English translation file!";
+    }
+    QCoreApplication::installTranslator(defaultLang);
+
+    QTranslator *translator = new QTranslator(app.data());
+    if (!translator->load(QLocale::system(), "harbour-beginner-guide", "-", TRANSLATION_INSTALL_DIR)) {
+        qWarning() << "Could not load translations for" << QLocale::system().name().toLatin1();
+    } else {
+        QCoreApplication::installTranslator(translator);
+    }
+
+    QScopedPointer<QQuickView> v(SailfishApp::createView());
     v->rootContext()->setContextProperty("settings", new AppSettings(app.data()));
 
     qmlRegisterType<Intl>("dev.chrastecky", 1, 0, "Intl");
